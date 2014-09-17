14b2e6a88d1852edd8ed48c15a8bdcf20a0e4469
Emily Chen <tenshiemi@gmail.com>
2014-09-17 11:24:43 +0200

    Added recursive array printer

--- pretty_printer.rb
+++ pretty_printer.rb
@@ -4,19 +4,20 @@ class PrintInput
 
 	def initialize(user_input)
 		@input_object = user_input
+    @level_child = 1
 	end
 
   def print
     if @input_object.is_a? Array
-      process_array
+      process_array(@input_object)
     else
       puts "I do not know how to print this object."
     end
   end
 
-	def process_array
-		puts "Array:"
-    @input_object.each do |element|
+	def process_array(array_for_print) 
+		puts "  "* (@level_child - 1) + "Array: level " + @level_child.to_s
+    array_for_print.each do |element|
 			if element.is_a? String
 				print_string(element)
 			elsif element.is_a? Numeric
@@ -33,26 +34,24 @@ class PrintInput
 	end
 
 	def print_string(string)
-		puts "  " + string
+		puts ("  " * @level_child) + string
 	end
 
   def print_number(number)
-   puts "  " + number.to_s
+   puts ("  " * @level_child) + number.to_s
   end
 
   def print_date(date)
-    puts "  " + date.to_s
-    # puts "  " + Date._strptime(date, '%d-%m-%Y').join
+    puts ("  " * @level_child) + date.to_s
   end
 
   def print_array(array)
-    puts "  Sub Array:"
-    array.each do | element|
-      puts "    " + element.to_s
-    end
+    @level_child += 1
+    process_array(array)
+    @level_child -= 1  
   end
 
 end
 
-array_data = ["hi", Date.today, 5, [1, 2, 3]]
+array_data = ["hi", Date.today, 5, [1, 2, 3, [3, 4, 5, [6, 7, 8]]], ["hi", "what's", "up"]]
 array = PrintInput.new(array_data).print
