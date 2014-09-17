92f20f7bb8f1357b26fed275ef33cc44c7121ac1
Emily Chen <tenshiemi@gmail.com>
2014-09-17 11:54:23 +0200

    added recursive hash processing

--- pretty_printer.rb
+++ pretty_printer.rb
@@ -8,7 +8,7 @@ class PrintInput
     @hash_level
 	end
 
-  def print
+  def print_pretty
     if @input_object.is_a? Array
       process_array(@input_object)
     else
@@ -19,21 +19,25 @@ class PrintInput
 	def process_array(array_for_print) 
 		puts "  "* (@level_child - 1) + "Array: level " + @level_child.to_s
     array_for_print.each do |element|
-			if element.is_a? String
-				print_string(element)
-			elsif element.is_a? Numeric
-        print_number(element)
-      elsif element.is_a? Date
-        print_date(element)
-			elsif element.is_a? Array
-        print_array(element)
-			elsif element.is_a? Hash
-        print_hash(element)
-      else
-        puts "I don't know how to print this object."
-			end
+			check_class(element)
 		end
 	end
+
+  def check_class(element)
+    if element.is_a? String
+        print_string(element)
+    elsif element.is_a? Numeric
+      print_number(element)
+    elsif element.is_a? Date
+      print_date(element)
+    elsif element.is_a? Array
+      print_array(element)
+    elsif element.is_a? Hash
+      print_hash(element)
+    else
+      puts "I don't know how to print this object."
+    end
+  end
 
 	def print_string(string)
 		puts ("  " * @level_child) + string
@@ -54,13 +58,17 @@ class PrintInput
   end
 
   def print_hash(hash)
+    @level_child += 1
     puts "  " * (@level_child) + "Hash: level " + @level_child.to_s
     hash.each do |key, value|
-      puts ("  " * (@level_child + 1)) + key.to_s + " --> " + value.to_s
+      print ("  " * (@level_child + 1)) + key.to_s + " -->" 
+      check_class(value)
     end
+    @level_child -= 1  
   end
 
 end
 
-array_data = ["hi", Date.today, 5, [1, 2, 3, [3, 4, 5, [6, 7, 8]]], ["hi", "what's", "up"], { :apple => [6, 7, 8], :oranges => 0, :grapes => 0, :banana => 0, :watermelon => 0 }]
-array = PrintInput.new(array_data).print
+array_data = ["hi", Date.today, 5, [1, 2, 3, [3, 4, 5, [6, "test7", {:oranges => "test1", :grapes => 2, :banana => 3}, {:oranges => 4, :grapes => 5, :banana => 6}]]], ["hi", "what's", "up"], { :apple => [6, 7, 8], :oranges => 7, :grapes => 8, :banana => 9 }]
+array = PrintInput.new(array_data).print_pretty
+
